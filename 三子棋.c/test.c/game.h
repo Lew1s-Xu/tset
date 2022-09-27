59fa0ef22d2ae1a6beea1c48281cc9993070da9d
Lew1s-Xu <tz_13772515591@163.com>
2022-09-27 20:22:15 +0800

    三子棋

---
+++ 三子棋.c/test.c/game.h
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#define ROW 3
+#define COL 3
+
+#include<stdio.h>
+//#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+//函数声明
+
+void InitBoard(char board[ROW][COL],int row,int col);
+
+void DisplayBoard(char board[ROW][COL], int row, int col);
+
+void PlayerMove(char board[ROW][COL], int row, int col);
+
+//四种状态
+//玩家win ‘*’
+//computer win '#'
+char Iswin(char board[ROW][COL], int row, int col);
